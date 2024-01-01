8a3aadc72f744517d43ac17be85ac887320b2062
chensgit169 <weichen191@mails.ucas.ac.cn>
2024-01-01 21:03:10 +0800

    chore: add gitignore, remove others' codes

--- dmrg/docs/ref/modules.rst
+++
@@ -1,7 +0,0 @@
-dmrg101
-=======
-
-.. toctree::
-   :maxdepth: 4
-
-   dmrg101
--- dmrg/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-File: __init__.py
-Author: Ivan Gonzalez
-Description: Imports the dmrg101 module
-'''
-import os
-import sys
-dmrg101_module_path=os.path.abspath('../dmrg101')
-sys.path.append(dmrg101_module_path)
