a7d04b9e7dbd71c67c643df75a0935392e6c9700
chen_wei <weichen191@mails.ucas.ac.cn>
2024-10-21 12:02:45 +0200

    add tight binding model

--- hamiltonian/tight_binding.py
+++ hamiltonian/tight_binding.py
@@ -1,42 +1,58 @@
 import numpy as np
 from numpy import ndarray
-from numpy.linalg import norm
 
 from hamiltonian.base import Hamiltonian
-from hamiltonian.operators import apply_ising_hamiltonian, apply_longitudinal_field, apply_transverse_field, apply_sigma
 
 
-class Hubbard(Hamiltonian):
+class TightBinding(Hamiltonian):
     def __init__(self, state_number: int,
-                 eps: ndarray,
-                 u: float = 1, use_pbc: bool = False):
+                 eps: ndarray | float | int,
+                 u: float = 1.,
+                 use_pbc: bool = False):
         """
         Args:
             state_number:
-            u: interaction constant, anti-ferromagnetic by default
+            eps: on-site energy, optionally varying with site
+            u: hopping constant, use as unit by default
             use_pbc: whether to use periodic boundary condition
         """
-        Hamiltonian.__init__(self, state_number, 1, u, use_pbc)
-        self.j = u
+        Hamiltonian.__init__(self, 1, state_number, use_pbc)
+        self.u = u
         self.eps = eps
-        if not len(eps) == state_number:
+
+        if isinstance(eps, (float, int)):
+            self.eps = np.array([eps] * state_number)
+        elif not len(eps) == state_number:
             raise ValueError("The length of eps should be equal to site_number.")
         self.use_pbc = use_pbc
 
     def _matvec(self, psi: np.ndarray) -> np.ndarray:
         """
-
+        Energy band + nearst hopping term
         """
-        psi_out = psi.copy()
-        for i_psi in range(self.n):
-            psi_out += self.eps[i_psi] * self.j * apply_sigma(psi, 0, i_psi)
-        for i_psi in range(self.n-1):
-            psi_out += self.j * apply_sigma(psi, 3, i_psi)
+        if not len(psi) == self.m:
+            raise ValueError("The length of psi should be equal to site_number.")
+
+        psi_out = psi * self.eps
+        psi_out[1:] += self.u * psi[:-1]
+        psi_out[:-1] += self.u * psi[1:]
+
+        if self.use_pbc:
+            psi_out[0] += self.u * psi[-1]
+            psi_out[-1] += self.u * psi[0]
 
         return psi_out
 
-    def apply_hopping(self, psi: np.ndarray, i: int, j: int):
-        psi_out = psi.copy()
-        psi_out += self.j * apply_sigma(psi, 1, i) * apply_sigma(psi, 0, j)
-        psi_out += self.j * apply_sigma(psi, 0, i) * apply_sigma(psi, 1, j)
-        return psi_out
+
+def test():
+    n = 20
+    e = 2
+    u = 0
+
+    model = TightBinding(n, e, u)
+    energy, psi = model.get_whole_spectrum()
+    assert np.allclose(energy, e * np.ones(n))
+
+
+if __name__ == '__main__':
+    test()
