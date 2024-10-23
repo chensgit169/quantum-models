684a028e51c7061404c708fe444b23e8422f6f5f
chen_wei <weichen191@mails.ucas.ac.cn>
2024-10-23 18:47:04 +0200

    set t-ticks

--- hamiltonian/visualization/time_evolution.py
+++ hamiltonian/visualization/time_evolution.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw_heatmap(ts_pn, ax=None):
+def draw_heatmap(ts_pn, ts=None, ax=None):
     """
     Visualize Pn(t) data in a heatmap.
     """
@@ -11,6 +11,8 @@ def draw_heatmap(ts_pn, ax=None):
         _, ax = plt.subplots()
 
     nt, nn = ts_pn.shape
+    if ts is None:
+        ts = np.arange(nt)
 
     # Draw a heatmap with the numeric values in each cell
     sns.heatmap(ts_pn, cmap='Blues', annot=False, cbar_kws={"shrink": 0.8}, ax=ax)
@@ -21,8 +23,16 @@ def draw_heatmap(ts_pn, ax=None):
     plt.xlabel('n')
     plt.ylabel('t')
 
+    # Set the x-axis labels to be the site number
+    Nt = 10
+    t_spacing = nt // Nt
+    t_ticks = list(range(0, nt, t_spacing))
+    print(t_ticks)
+
     ax.set_xticks(ticks=[i + 0.5 for i in range(nn)],
-                  labels=[str(i+1) for i in range(nn)])
+                  labels=[str(i + 1) for i in range(nn)])
+    ax.set_yticks(ticks=t_ticks,
+                  labels=[f'{ts[t]:.2f}' for t in t_ticks])
 
     ax.set_title('Time evolution of probability distribution $P_n(t)$')
     return ax
@@ -33,7 +43,7 @@ def test():
 
     # 生成一些随机数据
     data = np.random.rand(100, 20)
-    draw_heatmap(data, ax)
+    draw_heatmap(data, ax=ax)
     plt.savefig('time_evolution.png', dpi=400)
 
 
